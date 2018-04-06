c9a26476f79468b70eb90200b385fe8d2b5e4668
laszlopandy <laszlo.pandy@prezi.com>
2018-04-06 15:24:58 +0200

    Include all steps in build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+
+set -ex
+
+pushd hunspell
+autoreconf -vfi
+emconfigure ./configure
+emmake make
+popd
+
+rm -r embuild
+mkdir embuild
+
 em++ \
 	-O3 \
 	-Oz \
 	--llvm-lto 1 \
 	-s NO_EXIT_RUNTIME=1 \
 	-s EXPORTED_FUNCTIONS="['_Hunspell_create', '_Hunspell_destroy', '_Hunspell_spell', '_Hunspell_suggest', '_Hunspell_free_list']" \
-	../hunspell/src/hunspell/.libs/libhunspell-1.6.a \
-	-o hunspell.js
+	./hunspell/src/hunspell/.libs/libhunspell-1.6.a \
+	-o embuild/hunspell.js
