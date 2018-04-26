bbfa9db2b3c23e8575f830f9541dc146ca20df15
Marcelo Risoli <marcelo.risoli@prezi.com>
2018-04-26 15:12:34 +0200

    prepend license content as comments to the beginning of each js output file

--- build.sh
+++ build.sh
@@ -19,3 +19,6 @@ em++ \
 	-s EXPORTED_FUNCTIONS="['_Hunspell_create', '_Hunspell_destroy', '_Hunspell_spell', '_Hunspell_suggest', '_Hunspell_free_list']" \
 	./hunspell/src/hunspell/.libs/libhunspell-1.6.a \
 	-o embuild/hunspell.js
+
+cat LICENSE embuild/hunspell.js > tmp; mv tmp embuild/hunspell.js
+cat LICENSE embuild/hunspell.js.mem > tmp; mv tmp embuild/hunspell.js.mem
